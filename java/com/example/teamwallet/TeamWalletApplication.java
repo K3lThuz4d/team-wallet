a947258d2c9f8ee83f8fefb5619fc5989bfb7cfb
Chip <max.staerk1106@gmail.com>
2021-04-06 18:07:17 +0200

    initial commit

---
+++ java/com/example/teamwallet/TeamWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamWalletApplication.class, args);
+    }
+
+}
